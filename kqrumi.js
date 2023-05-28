82c82cb40eaffa852d1ea012934ecd3481c7abca
Kurumi <ayangkurumi@gmail.com>
2023-05-28 16:17:51 +0700

    Revert

--- kqrumi.js
+++ kqrumi.js
@@ -5607,26 +5607,6 @@ function init() {
 		<div id="content" class="nexmoe-item"></div>
 	 	<div id="readme_md" class="mdui-typo nexmoe-item" style="display:none; padding: 20px 0;"></div>
   </div>
-  <div id="disqus_thread"></div>
-  <script>
-	  /**
-	  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-	  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-	  /*
-	  var disqus_config = function () {
-	  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-	  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-	  };
-	  */
-	  (function() { // DON'T EDIT BELOW THIS LINE
-	  var d = document, s = d.createElement('script');
-	  s.src = 'https://kqrm.disqus.com/embed.js';
-	  s.setAttribute('data-timestamp', +new Date());
-	  (d.head || d.body).appendChild(s);
-	  })();
-  </script>
-  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-  <script id="dsq-count-scr" src="//kqrm.disqus.com/count.js" async></script>
   <div class="mdui-center mdui-text-center mdui-text-color-blue-grey-5001" style="margin-bottom: 20px">${UI.footer_text}</div>
   `;
 	if (UI.credits) {
